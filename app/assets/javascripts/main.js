49edf645dd1f068b99ab7baa454326f2f887a80f
Drew Moore <teloaster@yahoo.com>
2014-10-22 04:12:40 -0500

    Heads can now arrange in panorama mode.

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -7,19 +7,18 @@
   function initialize(){
     $(window).resize(resizing);
     //positionHeadsThreeColumns();
-    positionHeadsOneColumnCenter();
+    //positionHeadsOneColumnCenter();
+    positionHeadsPanorama();
   }
 
   function resizing() {
   }
 
   function positionHeadsOneColumnCenter() {
-    //$('div[class^="fold"] div[class^="panel"]').css('position', 'static');
     $('#folds-container').removeClass('full-screen');
     $('#folds-container').addClass('three-quarters-screen');
     $('#folds-container').css('left', '33%');
     $('div[class^="panel"]').width('100%');
-    //$('div[class^="fold"]]').css('left': '25%')
   }
 
   function positionHeadsThreeColumns() {
@@ -28,14 +27,9 @@
     $('.foldC').animate({'left': '25%'}, 1500);
     $('.foldR').animate({'left': '50%'}, 1500);
     _.each(folds, function(fold){
-      //var $left = $($('.panelL')[0]);
       var $left = $(fold).find('.panelL');
-      //var $center = $($('.panelC')[0]);
       var $center = $(fold).find('.panelC');
-      //var $right = $($('.panelR')[0]);
       var $right = $(fold).find('.panelR');
-
-      //var viewWidth = $center.parent().width();
       var viewWidth = $(fold).width();
 
       var panelWidth = $center.width();
@@ -49,7 +43,32 @@
       $right.css('left', rightAnchor.toString() + 'px');
     });
 
+
     //console.log('positionHeads: ', centerLeftAnchor, centerRightAnchor);
+  }
+
+  function positionHeadsPanorama(){
+    $('div[class^="panel"]').width('50%');
+    var folds = $('[class^="fold"]');
+    $('.foldL').animate({'left': '0%'}, 1500);
+    $('.foldC').animate({'left': '25%'}, 1500);
+    $('.foldR').animate({'right': '0%'}, 1500);
+    _.each(folds, function(fold){
+      var $left = $(fold).find('.panelL');
+      var $center = $(fold).find('.panelC');
+      var $right = $(fold).find('.panelR');
+      var viewWidth = $(fold).width();
+
+      var panelWidth = $center.width();
+      var centerLeftAnchor = (viewWidth / 2) - (panelWidth / 2);
+      var leftAnchor = centerLeftAnchor - (panelWidth * 0.33);
+      var centerRightAnchor = (viewWidth / 2) + (panelWidth / 2);
+      var rightAnchor = centerRightAnchor - (panelWidth * 0.66);
+
+      $center.css('left', centerLeftAnchor.toString() + 'px');
+      $left.css('left', leftAnchor.toString() + 'px');
+      $right.css('left', rightAnchor.toString() + 'px');
+    });
   }
 
 })();
