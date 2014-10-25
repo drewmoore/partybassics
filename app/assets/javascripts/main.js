2d03d6351d0c5dbfdd2adada931e5da08857b387
Drew Moore <teloaster@yahoo.com>
2014-10-25 15:21:15 -0500

    Improved animations and centering.

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -30,7 +30,7 @@
     //$('#folds-container').removeClass('two-thirds-screen');
     //$('#folds-container').addClass('full-screen');
     //$('#folds-container').css('left', '0%');
-    $('div[class^="panel"]').width('33%');
+    $('div[class^="panel"]').animate({'width':'33%'});
     var folds = $('[class^="fold"]');
     $('.foldL').animate({'left': '0%'}, 1500);
     $('.foldC').animate({'left': '25%'}, 1500);
@@ -47,9 +47,9 @@
       var centerRightAnchor = (viewWidth / 2) + (panelWidth / 2);
       var rightAnchor = centerRightAnchor - (panelWidth * 0.66);
 
-      $center.css('left', centerLeftAnchor.toString() + 'px');
-      $left.css('left', leftAnchor.toString() + 'px');
-      $right.css('left', rightAnchor.toString() + 'px');
+      $left.css('left', '25%');
+      $center.css('left', '37%');
+      $right.css('left', '50%');
     });
 
 
@@ -57,29 +57,29 @@
   }
 
   function positionHeadsPanorama(){
-    $('#folds-container').removeClass('two-thirds-screen');
-    $('#folds-container').addClass('full-screen');
-    $('#folds-container').animate({'left': '0%'});
-    $('div[class^="panel"]').width('50%');
-    var folds = $('[class^="fold"]');
+    //$('#folds-container').removeClass('two-thirds-screen');
+    //$('#folds-container').addClass('full-screen');
+    //$('#folds-container').animate({'left': '0%'});
     $('.foldL').animate({'left': '0%'}, 1500);
     $('.foldC').animate({'left': '25%'}, 1500);
     $('.foldR').animate({'left': '50%'}, 1500);
+    $('div[class^="panel"]').animate({'width':'50%'});
+    var folds = $('[class^="fold"]');
     _.each(folds, function(fold){
       var $left = $(fold).find('.panelL');
       var $center = $(fold).find('.panelC');
       var $right = $(fold).find('.panelR');
-      var viewWidth = $(fold).width();
+      //var viewWidth = $(fold).width();
 
-      var panelWidth = $center.width();
-      var centerLeftAnchor = (viewWidth / 2) - (panelWidth / 2);
-      var leftAnchor = centerLeftAnchor - (panelWidth * 0.33);
-      var centerRightAnchor = (viewWidth / 2) + (panelWidth / 2);
-      var rightAnchor = centerRightAnchor - (panelWidth * 0.66);
+      //var panelWidth = $center.width();
+      //var centerLeftAnchor = (viewWidth / 2) - (panelWidth / 2);
+      //var leftAnchor = centerLeftAnchor - (panelWidth * 0.33);
+      //var centerRightAnchor = (viewWidth / 2) + (panelWidth / 2);
+      //var rightAnchor = centerRightAnchor - (panelWidth * 0.66);
 
-      $center.css('left', centerLeftAnchor.toString() + 'px');
-      $left.css('left', leftAnchor.toString() + 'px');
-      $right.css('left', rightAnchor.toString() + 'px');
+      $left.css('left', '0%');
+      $center.css('left', '25%');
+      $right.css('left', '50%');
     });
   }
 
