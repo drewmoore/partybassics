be0f1596a13891466261955fa427ad62f8f7c108
Drew Moore <teloaster@yahoo.com>
2014-11-18 14:47:58 -0600

    Improvements on homepage styling. Placeholder buttons now styled links.

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -10,29 +10,19 @@
   }
 
   function initializeEventHandlers() {
-    $('#three-column').click(positionHeadsThreeColumns);
-    $('#panorama-column').click(positionHeadsPanorama);
+    $('.three-column-button').click(positionHeadsThreeColumns);
+    $('.panorama-column-button').click(positionHeadsPanorama);
     $('.content-panel').mouseenter(tryptGlow);
     $('.content-panel').mouseleave(tryptFade);
     $('#facebook-event').click(getFacebookEvent);
   }
 
   function positionHeadsOneColumnCenter() {
-    /*
-    $('#folds-container').removeClass('full-screen');
-    $('#folds-container').addClass('two-thirds-screen');
-    $('#folds-container').css('left', '33%');
-    $('div[class^="panel"]').width('100%');
-    */
     $('div[class^="fold"]').css('left', '33%');
     $('div[class^="panel"]').width('75%');
-
   }
 
   function positionHeadsThreeColumns() {
-    //$('#folds-container').removeClass('two-thirds-screen');
-    //$('#folds-container').addClass('full-screen');
-    //$('#folds-container').css('left', '0%');
     $('div[class^="panel"]').animate({'width':'33%'});
     var folds = $('[class^="fold"]');
     $('.foldL').animate({'left': '0%'}, 1500);
@@ -42,27 +32,13 @@
       var $left = $(fold).find('.panelL');
       var $center = $(fold).find('.panelC');
       var $right = $(fold).find('.panelR');
-      //var viewWidth = $(fold).width();
-
-      //var panelWidth = $center.width();
-      //var centerLeftAnchor = (viewWidth / 2) - (panelWidth / 2);
-      //var leftAnchor = centerLeftAnchor - (panelWidth * 0.33);
-      //var centerRightAnchor = (viewWidth / 2) + (panelWidth / 2);
-      //var rightAnchor = centerRightAnchor - (panelWidth * 0.66);
-
       $left.css('left', '25%');
       $center.css('left', '37%');
       $right.css('left', '50%');
     });
-
-
-    //console.log('positionHeads: ', centerLeftAnchor, centerRightAnchor);
   }
 
   function positionHeadsPanorama(){
-    //$('#folds-container').removeClass('two-thirds-screen');
-    //$('#folds-container').addClass('full-screen');
-    //$('#folds-container').animate({'left': '0%'});
     $('.foldL').animate({'left': '0%'}, 1500);
     $('.foldC').animate({'left': '25%'}, 1500);
     $('.foldR').animate({'left': '50%'}, 1500);
@@ -72,14 +48,6 @@
       var $left = $(fold).find('.panelL');
       var $center = $(fold).find('.panelC');
       var $right = $(fold).find('.panelR');
-      //var viewWidth = $(fold).width();
-
-      //var panelWidth = $center.width();
-      //var centerLeftAnchor = (viewWidth / 2) - (panelWidth / 2);
-      //var leftAnchor = centerLeftAnchor - (panelWidth * 0.33);
-      //var centerRightAnchor = (viewWidth / 2) + (panelWidth / 2);
-      //var rightAnchor = centerRightAnchor - (panelWidth * 0.66);
-
       $left.css('left', '0%');
       $center.css('left', '25%');
       $right.css('left', '50%');
@@ -179,12 +147,5 @@
     }
     return newMonth;
   }
-
-
-  /*
-  function facebookError(obj, httpStatus, err){
-    console.log(obj, httpStatus, err);
-  }
-  */
 
 })();
