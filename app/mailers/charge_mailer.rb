450db82977980b67933a1ed97b654c5c7f7720f7
Drew Moore <teloaster@yahoo.com>
2014-12-06 16:12:39 -0600

    Content Management implemented in views and in purchase confirmation email. Email still using Page model, might create similar Email model.

--- app/mailers/charge_mailer.rb
+++ app/mailers/charge_mailer.rb
@@ -1,14 +1,17 @@
 class ChargeMailer < ActionMailer::Base
-  default from: "partybassics@partybassics.com"
+  default from: "partybassics@gmail.com"
   layout 'email'
   add_template_helper(EventsHelper)
 
   def purchase_confirmation(purchase, event)
     @purchase = purchase
     @event = event
-
-    #attachments.inline["logo-dark-email.png"] = File.read("/data/ruby/partybassics/app/assets/images/logo-dark-email.png")
-
-    mail(to: @purchase.email, subject: "Ticket Confirmation from Party Bassics")
+    @page = Page.find_by("controller LIKE ? and action LIKE ?", "mailers", "purchase_confirmation")
+    @graphics = @page.get_graphics
+    @page.graphics.each do |graphic|
+      image_file = graphic.image.current_path.to_s
+      attachments.inline[graphic.image.file.file] = File.read image_file
+    end
+    mail(to: @purchase.email, subject: @page.title)
   end
 end
--- app/models/page.rb
+++ app/models/page.rb
@@ -2,4 +2,13 @@ class Page < ActiveRecord::Base
   validates_uniqueness_of :controller, :scope => :action
   has_and_belongs_to_many :contents, :join_table => :contents_pages
   has_and_belongs_to_many :graphics, :join_table => :graphics_pages
+
+  def get_graphics
+    graphics = {}
+    self.graphics.each do |g|
+      graphics[g.identifier] = g
+    end
+    return graphics
+  end
+
 end
