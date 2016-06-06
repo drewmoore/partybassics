0c6a08a840aa45edc22d83bec4b2f63cc077bb4a
Drew Moore <teloaster@yahoo.com>
2016-06-06 01:37:34 -0500

    Switched event flyer to s3.

--- app/uploaders/flyer_uploader.rb
+++ app/uploaders/flyer_uploader.rb
@@ -7,6 +7,10 @@ class FlyerUploader < CarrierWave::Uploader::Base
   # include CarrierWave::RMagick
   # include CarrierWave::MiniMagick
 
+  # Choose what kind of storage to use for this uploader:
+  # storage :file
+  storage :fog
+
   version :landscape, :if => :is_landscape? do
     process :resize_to_fit => [800, 533]
     version :display do
@@ -34,16 +38,11 @@ class FlyerUploader < CarrierWave::Uploader::Base
       image[:height] > image[:width]
     end
 
-
-  # Choose what kind of storage to use for this uploader:
-  storage :file
-  # storage :fog
-
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
-  def store_dir
-    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
-  end
+  # def store_dir
+  #   "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  # end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
   # def default_url
