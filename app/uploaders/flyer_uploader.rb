4f55dc2cd5b2d165091161c53b6a51f31637e477
Drew Moore <teloaster@yahoo.com>
2016-06-06 02:02:51 -0500

    ImageMagick s3 points direct to flyer image.

--- app/uploaders/flyer_uploader.rb
+++ app/uploaders/flyer_uploader.rb
@@ -28,12 +28,12 @@ class FlyerUploader < CarrierWave::Uploader::Base
   protected
 
     def is_landscape? picture
-      image = MiniMagick::Image.open(picture.file.url)
+      image = MiniMagick::Image.open(picture)
       image[:width] > image[:height]
     end
 
     def is_portrait? picture
-      image = MiniMagick::Image.open(picture.file.url)
+      image = MiniMagick::Image.open(picture)
       image[:height] > image[:width]
     end
 
