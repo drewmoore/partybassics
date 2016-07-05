f169e38a2d0b2757c2447db5c3838e9f7321f3e3
Drew Moore <teloaster@yahoo.com>
2016-07-05 10:46:07 -0500

    Switched event mailer over to using new flyer partial.

--- app/mailers/custom_mailer.rb
+++ app/mailers/custom_mailer.rb
@@ -24,11 +24,6 @@ class CustomMailer < ActionMailer::Base
     @layout_contents = @layout.get_contents
     @contents = @page.get_contents
     @graphics = @layout.get_graphics
-    if File.exists? event.flyer.versions[:portrait].display.path
-      image_file = @event.flyer.versions[:portrait].display.current_path.to_s
-    elsif File.exists? event.flyer.versions[:landscape].display.path
-      image_file = @event.flyer.versions[:landscape].display.current_path.to_s
-    end
     main_email = @layout_contents["contact-info-content-main-email"].text
     mail(subject: @subject, to: @contact.email, from: main_email).deliver
   end
